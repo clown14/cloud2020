ff291a5dd9acb508d8b04e5653476070a5e56182
clown <785613198@qq.com>
2020-07-16 19:50:42 +0800

    first commit

---
+++ cloud-provider-payment8001/src/main/java/com/atguigu/springcloud/PaymentMain8001.java
@@ -0,0 +1,19 @@
+package com.atguigu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @program: cloud2020
+ * @author: onion
+ * @create: 2020-07-16 19:42
+ **/
+
+@SpringBootApplication
+public class PaymentMain8001 {
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentMain8001.class, args);
+    }
+}
+
+    
