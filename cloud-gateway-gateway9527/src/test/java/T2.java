e8b718fa3466dfde88dcc9bff108e65601d572a1
clown <785613198@qq.com>
2020-08-16 23:04:45 +0800

    add Gateway Predicates,Filter

---
+++ cloud-gateway-gateway9527/src/test/java/T2.java
@@ -0,0 +1,17 @@
+import java.time.ZonedDateTime;
+
+/**
+ * @program: cloud2020
+ * @author: onion
+ * @create: 2020-08-16 22:20
+ **/
+
+public class T2 {
+    public static void main(String[] args) {
+        ZonedDateTime zbj = ZonedDateTime.now();
+        System.out.println(zbj);
+
+    }
+}
+
+    
