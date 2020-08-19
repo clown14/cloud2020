8a746cd1c2344a41a819b612e8405ef95c277c22
clown <785613198@qq.com>
2020-08-19 15:14:24 +0800

    add Bus

---
+++ cloud-config-client-3366/src/main/java/com/atguigu/springcloud/ConfigClientMain3366.java
@@ -0,0 +1,22 @@
+package com.atguigu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @program: cloud2020
+ * @author: onion
+ * @create: 2020-08-18 16:37
+ **/
+
+@SpringBootApplication
+@EnableEurekaClient
+public class ConfigClientMain3366 {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigClientMain3366.class, args);
+    }
+
+}
+
+    
---
+++ cloud-config-client-3366/src/main/java/com/atguigu/springcloud/controller/ConfigClientController.java
@@ -0,0 +1,29 @@
+package com.atguigu.springcloud.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @program: cloud2020
+ * @author: onion
+ * @create: 2020-08-18 16:38
+ **/
+
+@RestController
+@RefreshScope
+public class ConfigClientController {
+    @Value("${config.info}")
+    private String configInfo;
+
+    @Value("${server.port}")
+    private String serverPort;
+
+    @GetMapping("/configInfo")
+    public String getConfigInfo() {
+        return "serverPort: " + serverPort + "\t\n\n configInfo: " + configInfo;
+    }
+}
+
+    
