5994f673d0aff1eed578c117dc2d8f54f5ee9297
clown <785613198@qq.com>
2020-08-25 14:58:39 +0800

    add Nacos 服务配置中心

---
+++ cloudalibaba-config-nacos-client3377/src/main/java/com/atguigu/springcloud/alibaba/NacosConfigClientMain3377.java
@@ -0,0 +1,21 @@
+package com.atguigu.springcloud.alibaba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @program: cloud2020
+ * @author: onion
+ * @create: 2020-08-24 15:45
+ **/
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class NacosConfigClientMain3377 {
+    public static void main(String[] args) {
+        SpringApplication.run(NacosConfigClientMain3377.class, args);
+    }
+}
+
+    
