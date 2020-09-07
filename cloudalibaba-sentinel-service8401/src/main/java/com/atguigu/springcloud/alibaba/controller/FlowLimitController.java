1b7639cf37c4c4ba98ae8e0edb4a0d4a5ed978ec
clown <785613198@qq.com>
2020-09-07 23:37:12 +0800

    add Sentinel 降级

--- cloudalibaba-sentinel-service8401/src/main/java/com/atguigu/springcloud/alibaba/controller/FlowLimitController.java
+++ cloudalibaba-sentinel-service8401/src/main/java/com/atguigu/springcloud/alibaba/controller/FlowLimitController.java
@@ -1,5 +1,6 @@
 package com.atguigu.springcloud.alibaba.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,6 +13,7 @@ import java.util.concurrent.TimeUnit;
  **/
 
 @RestController
+@Slf4j
 public class FlowLimitController {
 
     @GetMapping("/testA")
@@ -26,7 +28,34 @@ public class FlowLimitController {
 
     @GetMapping("/testB")
     public String testB() {
+        log.info(Thread.currentThread().getName() + "\t" + "....testB");
         return "-----testB";
+    }
+
+    @GetMapping("/testD")
+    public String testD() {
+        try {
+            TimeUnit.SECONDS.sleep(1);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        log.info("testD 测试RT");
+        return "-----testD";
+    }
+
+    @GetMapping("/testC")
+    public String testC() {
+
+        log.info("testC 测试异常比例");
+        int age = 10 / 0;
+        return "-----testC";
+    }
+
+    @GetMapping("/testE")
+    public String testE() {
+
+        log.info("testC 测试异常数");
+        return "-----testE";
     }
 }
 
